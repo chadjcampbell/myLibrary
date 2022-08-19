f2742bcf113b81cd4dcf8e2c2271aad2bdb6f1ef
Chad <chadjcampbell@gmail.com>
2022-08-19 14:32:31 -0400

    all of addBook base functions and cards display

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let myLibrary = [];
+let booknumber = 0;
 
-function Book(title, author, pages, read) {
+
+function Book(title, author, pages, read, booknumber) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.booknumber = booknumber;
 }
 
 function addBookToLibrary() {
@@ -14,12 +17,33 @@ function addBookToLibrary() {
     let author = formData.get('author');
     let pages = formData.get('pages');
     let read = formData.get('read');
-    myLibrary.push(new Book(title, author, pages, read));
+    myLibrary.push(new Book(title, author, pages, read, booknumber));
+    booknumber++;
     addform.reset();
+    renderLibrary();
 }
 
 function renderLibrary() {
-    for (Book in myLibrary) {
-        
+    const books = document.querySelector('#books');
+        books.innerHTML = '';
+    for (let x in myLibrary) {
+        let checked = '';
+        if (myLibrary[x].read == 1) {
+            checked = 'checked'
+        }
+        books.innerHTML += `<div class="card">
+        <div class="card-header">
+          ${myLibrary[x].title}
+        </div>
+        <div class="card-body">
+          <h7 class="card-title">${myLibrary[x].author}</h7>
+          <p class="card-text">Pages: ${myLibrary[x].pages} </p>
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" name="cardread" id="cardread" ${checked}>
+            <label class="form-check-label" for="cardread">Read</label>
+          </div>
+          <button class="remove btn btn-primary">Remove</button>
+        </div>
+      </div>`
     }
 }
