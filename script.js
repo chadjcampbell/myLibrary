0a81ef26ca0110ddb1a5d2f58da3a535ffa72243
Chad <chadjcampbell@gmail.com>
2022-09-21 15:18:54 -0400

    added simple form validation

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 let myLibrary = [];
 let booknumber = 0;
 
-
 class Book {
-    constructor(title, author, pages, read, booknumber) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-        this.booknumber = booknumber;
-    }
+  constructor(title, author, pages, read, booknumber) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.booknumber = booknumber;
+  }
 }
 
 function addBookToLibrary() {
-    let addform = document.getElementById('addform');
-    let formData = new FormData(addform);
-    let title = formData.get('title');
-    let author = formData.get('author');
-    let pages = formData.get('pages');
-    let read = formData.get('read');
-    myLibrary.push(new Book(title, author, pages, read, booknumber));
-    booknumber++;
-    addform.reset();
-    renderLibrary();
+  let addform = document.getElementById("addform");
+  let formData = new FormData(addform);
+  let title = formData.get("title");
+  let author = formData.get("author");
+  let pages = formData.get("pages");
+  let read = formData.get("read");
+  if (!addform.reportValidity()) {
+    return;
+  }
+  myLibrary.push(new Book(title, author, pages, read, booknumber));
+  booknumber++;
+  addform.reset();
+  renderLibrary();
 }
 
 function renderLibrary() {
-    const books = document.querySelector('#books');
-        books.innerHTML = '';
-    for (let x in myLibrary) {
-        let checked = '';
-        if (myLibrary[x].read == 1) {
-            checked = 'checked'
-        }
-        books.innerHTML += `<div class="card">
+  const books = document.querySelector("#books");
+  books.innerHTML = "";
+  for (let x in myLibrary) {
+    let checked = "";
+    if (myLibrary[x].read == 1) {
+      checked = "checked";
+    }
+    books.innerHTML += `<div class="card">
         <div class="card-header">
           ${myLibrary[x].title}
         </div>
@@ -46,27 +48,26 @@ function renderLibrary() {
           </div>
           <button data-id="${myLibrary[x].booknumber}" onclick="remove(this)" class="remove btn btn-primary">Remove</button>
         </div>
-      </div>`
-    }
+      </div>`;
+  }
 }
 
 function readornot(element) {
-    let id = element.dataset.id;
-    let read = element.dataset.read;
-    if (read == 1) {
-        myLibrary[id].read = 2;
-    }
-    else {
-        myLibrary[id].read = 1;
-    }
-    renderLibrary();
+  let id = element.dataset.id;
+  let read = element.dataset.read;
+  if (read == 1) {
+    myLibrary[id].read = 2;
+  } else {
+    myLibrary[id].read = 1;
+  }
+  renderLibrary();
 }
 
 function remove(element) {
-    let id = element.dataset.id;
-    let indexOfBook = myLibrary.findIndex (book => {
-        return book.booknumber == id;
-    });
-    myLibrary.splice(indexOfBook, 1);
-    renderLibrary();
+  let id = element.dataset.id;
+  let indexOfBook = myLibrary.findIndex((book) => {
+    return book.booknumber == id;
+  });
+  myLibrary.splice(indexOfBook, 1);
+  renderLibrary();
 }
